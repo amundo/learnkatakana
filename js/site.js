7e2f85a3d71926229a3d69a940a5fd1d6418bddc
Patrick Hall <pat@amundo.com>
2010-11-02 15:58:56 -0700

    added crude flickr search

--- js/site.js
+++ js/site.js
@@ -1,3 +1,34 @@
+
+function searchFlickr(query){
+  var $thumbs = $('#thumbs');
+  var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + query + "&tagmode=any&ss=1&format=json&l=commderiv&jsoncallback=?"
+  $.getJSON(url,
+    function(data){
+      $thumbs.html('<h1>\u3010' + query + '\u3011</h1><h2>free...</h2>');
+      if(data.items.length == 0) { $thumbs.html('<h1>sorry no free pics for \u3010' + query + '\u3011 :(</h1>')} 
+      $.each(data.items, function(i,item){
+        var img = $('<img class="thumb" />').attr({"author": item.author, "src": item.media.m, "href": item.link}).appendTo($thumbs);
+        //img.appendTo($thumbs);
+        if ( i == 40 ) return false;
+      });
+   });
+
+  var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + query + "&tagmode=any&ss=1&format=json&jsoncallback=?"
+  $.getJSON(url,
+    function(data){
+      $thumbs.append('<hr><h1>\u3010' + query + '\u3011</h1><h2>unfree...</h2>');
+      if(data.items.length == 0) { $thumbs.append('<h1>sorry no unfree pics for \u3010' + query + '\u3011 :(</h1>')} 
+      $.each(data.items, function(i,item){
+        var img = $('<img class="thumb" />').attr({"author": item.author, "src": item.media.m, "href": item.link}).appendTo($thumbs);
+        //img.appendTo($thumbs);
+        if ( i == 40 ) return false;
+      });
+   });
+}
+
+
+
+
 $(function(){
 
   function convert(text, rules){
@@ -23,5 +54,11 @@ $(function(){
   })
 
   $('ol#level').shuffle();
+  $('<div/>', { 'id': 'thumbs', }).css({'float':'right','width' : '40%'}).appendTo('body');
+  $('ol#level').delegate('li', 'click', function(ev){
+   var query = $(this).text();
+   searchFlickr(query); 
+   ev.preventDefault();
+   });
 
 })
